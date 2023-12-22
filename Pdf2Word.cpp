234b02f8920176ea25eb5e2eada7b3ee42023c06
MainGift <352823321@qq.com>
2023-12-22 21:04:07 +0800

    UnAlpha.Bugfix.003

--- Pdf2Word.cpp
+++ Pdf2Word.cpp
@@ -17,26 +17,29 @@ Pdf2Word::~Pdf2Word()
 void Pdf2Word::on_filename_clicked() {
 	QString qpath = QFileDialog::getOpenFileName(this, "Maingift\345\267\245\345\205\267\347\256\261", 
 		QApplication::applicationDirPath(), filter.join(";;"));
-	std::wstring str = qpath.toStdWString();
-	LPCWSTR path = str.c_str();
 
 	ui.filename_edit->clear();
 	ui.filename_edit->insert(qpath);
 
-	pdf = new Spire::Pdf::PdfDocument();
-	pdf->LoadFromFile(path);
+	if (qpath == "") return;
 }
 
 void Pdf2Word::on_output_clicked() {
+	QString filename = ui.filename_edit->text(); 
+	std::wstring fstr = filename.toStdWString();
+	LPCWSTR fpath = fstr.c_str();
+
+	pdf = new Spire::Pdf::PdfDocument();
+	pdf->LoadFromFile(fpath);
+
 	QString qpath = QFileDialog::getSaveFileName(this, "Maingift\345\267\245\345\205\267\347\256\261", 
 		QApplication::applicationDirPath(), "Word\346\226\207\346\241\243 (*.doc *.docx)");
 	std::wstring str = qpath.toStdWString();
 	LPCWSTR path = str.c_str();
 
-	if (!(path)) return ;
 	if (pdf == nullptr) {
 		QMessageBox showerror(QMessageBox::Warning, "Maingift\345\267\245\345\205\267\347\256\261", 
-			"\346\227\240\346\263\225\346\211\223\345\274\200\346\226\207\344\273\266", QMessageBox::Ok);
+			"Ooops......\346\227\240\346\263\225\346\211\223\345\274\200\346\226\207\344\273\266", QMessageBox::Ok);
 		showerror.exec();
 		return ;
 	}
